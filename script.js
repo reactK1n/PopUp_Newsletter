c414239e253eda89e9f2a506bd0cd8dbe8039bef
reactK1n <adewings1995@gmail.com>
2022-09-28 11:56:30 +0100

    just changes

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ let cancel = document.querySelector(".cancel");
 // The pop up should display when the system is loading
 window.addEventListener("load", ()=>{
     popUp.style.transform=" translateY(0px)";
+
+      // Clear error after 3 seconds
+  setTimeout(clearPop, 3000);
+
 });
+
+// Clear error
+function clearPop(){
+  popUp.remove();
+}
 
 // The pop up should disappear when icon cancel is clicked
 cancel.addEventListener("click", ()=>{
